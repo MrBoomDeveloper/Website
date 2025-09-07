c6e564383142ff3b898178050bf7f900ba2b1ce6
MrBoomDev <github@mrboomdev.ru>
2025-09-07 12:14:06 +0500

    Initial commit

---
+++ astro.config.ts
@@ -0,0 +1,17 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+	site: "https://mrboomdev.ru",
+	trailingSlash: "ignore",
+
+	// i18n: {
+	// 	locales: ["en", "ru"],
+	// 	defaultLocale: "en",
+
+	// 	routing: {
+	// 		prefixDefaultLocale: true
+	// 	}
+	// }
+});
